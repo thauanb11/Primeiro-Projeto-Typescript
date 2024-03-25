0ea6f61efb24b523ec9811a869861ae6227a86fb
thauanb11 <guedesbruno40@gmail.com>
2024-03-25 12:47:21 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import express from 'express'
+import rotas from './rotas'
+
+const app = express()
+
+app.use (express.json())
+app.use(rotas)
+
+app.listen('3000', ()=>{
+    console.log('Servidor rodando na porta 3000')
+})
